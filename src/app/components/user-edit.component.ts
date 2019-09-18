5e3456ac9e5b34122561962ec647db8f469cb48a
LesOswald <lesoswald311@gmail.com>
2019-09-18 18:05:12 -0500

    Maquetado Barra

--- src/app/components/user-edit.component.ts
+++ src/app/components/user-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GLOBAL } from '../services/global';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
 import { stringify } from 'querystring';
@@ -16,6 +17,7 @@ export class UserEditComponent implements OnInit{
     public identity;
     public token;
     public alertMessage;
+    public url: string;
 
     constructor(
         private _userService:UserService
@@ -24,6 +26,7 @@ export class UserEditComponent implements OnInit{
         this.identity = this._userService.getIdentity();
         this.token = this._userService.getToken();
         this.user = this.identity;
+        this.url = GLOBAL.url;
     
     }
 
@@ -33,18 +36,34 @@ export class UserEditComponent implements OnInit{
     }
 
     onSubmit(){
-
         console.log(this.user);
         console.log("USUARIO INGRESADO", this.user);
         this._userService.updateUser(this.user).subscribe(
             response => {
-                console.log("TCL: UserEditComponent -> onSubmit -> response", response)
+                console.log("TCL: UserEditComponent -> onSubmit -> response", response);
                 if(!response.user){
                     this.alertMessage = 'Error Al Actualizar El Usuario';
 
                 } else{
-                    this.user = response.user;
+                    //this.user = response.user;
                     localStorage.setItem('identity', JSON.stringify(this.user));
+                    //document.getElementById("identity_name").innerHTML = this.user.name;
+
+                    if(!this.filesToUpload){
+                        // Redirección
+
+                    } else{
+                        this.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload).then(
+                            (result: any) => {
+                                this.user.image = result.image;
+                                localStorage.setItem('identity', JSON.stringify(this.user));
+                                let image_path = this.url+'get-image-user/'+this.user.image;
+                                document.getElementById('image-logged').setAttribute('src', image_path);
+
+                            }
+                        );
+                    }
+
                     this.alertMessage = 'Usuario Actualizado Correctamente';
 
                 }
@@ -67,6 +86,7 @@ export class UserEditComponent implements OnInit{
 
     fileChangeEvent(fileInput:any){
         this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log("TCL: UserEditComponent -> fileChangeEvent -> this.filesToUpload", this.filesToUpload);
 
     }
 
@@ -74,9 +94,29 @@ export class UserEditComponent implements OnInit{
          var token = this.token;
          
          return new Promise(function(resolve, reject){
-             
+             var formData:any = new FormData();
+             var xhr = new XMLHttpRequest();
+
+             for(var i =0; i < files.length; i++){
+                 formData.append('image', files[i], files[i].name);
+             }
+
+             xhr.onreadystatechange = function(){
+                 if(xhr.readyState == 4){
+                     if(xhr.status == 200){
+                         resolve(JSON.parse(xhr.response));
+
+                     } else{
+                         reject(xhr.response)
+                     }
+                 }
+             }
+
+             xhr.open('POST', url, true);
+             xhr.setRequestHeader('Authorization', token);
+             xhr.send(formData);
+
          })
     }
-
 }
 
